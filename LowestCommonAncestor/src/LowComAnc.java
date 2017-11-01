60418b9178bcd464dc35076c53c81ed7229a288e
Evan O'Sullivan <osulliev@tcd.ie>
2017-11-01 21:43:12 +0000

    Added some test files with a changed DAG program

--- LowestCommonAncestor/src/LowComAnc.java
+++ LowestCommonAncestor/src/LowComAnc.java
@@ -1,57 +1,154 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
-//source: https://www.youtube.com/watch?v=fjT3WDKiAkI
+public class LowComAnc {
+    public static final int UNUSED = 0;
+    public static final int USING= 1;
+    public static final int COMPLETE = 2;
 
-class Graph{
-	class Edge{
-		int v,w;
-		public Edge(int v, int w){
-			this.v=v; this.w=w;
-		}
-		@Override
-		public String toString(){
-			return "(" + v +","+w+")";
-		}
-	}
-	List<Edge> g[];
-	public Graph(int n){
-		g=new LinkedList[n];
-		for(int i =0;i<g.length;i++){
-			g[i]=new LinkedList<Edge>();
-		}
-	}
-	boolean isConnected(int u, int v){
-		for(Edge i: g[u]){
-			if(i.v==v) return true;}
-			return false;
-	}
-	
-	void addEdge(int u ,int v, int w){
-		g[u].add(0, new Edge(v,w));
-	}
-	
-	@Override
-	public String toString(){
-		String result = "";
-		for(int i = 0 ; i < g.length;i++){
-			result+=i+"=>"+g[i] + "\n";
-		}
-		return result;
-			
-	}
-}
-public class LowComAnc{
-	public static void main (String[] args){
-		Graph G = new Graph(10);
-		G.addEdge(0,2,10);
-		G.addEdge(0,5,15);
-		G.addEdge(2,5,10);
-		G.addEdge(9,3,16);
-		
-		
-		System.out.println(G);;
-		System.out.println(G.isConnected(2,3));
-		System.out.println(G.isConnected(9,16));
-		
-	}
+    private final ArrayList<Integer>[] graph;  
+  
+    public void Edge(int v, int w) {
+        if (v >= 0 && v < graph.length && w >= 0 && w < graph.length)
+            Edge(graph, v, w);
+        else System.out.println("Edge " + v + "->" + w + " ignored due to non-existent vertex.");
+    }
+
+    
+    private void Edge(ArrayList<Integer>[] graph, int v, int w) {
+        ArrayList<Integer> adjList = graph[v]; 
+        if (!adjList.contains(w)) {
+            adjList.add(w);
+            if (containsCycle()) {
+                for (int i = 0; i < adjList.size(); i++) {
+                    if (adjList.get(i) == w)
+                        adjList.remove(i);
+                }
+                System.out.println("Cycle-completing edge " + v + "->" + w + " ignored.");
+            }
+        }
+        else System.out.println("Duplicate edge " + v + "->" + w + " ignored.");
+    }
+
+    public LowComAnc(int v) {
+        graph = (ArrayList<Integer>[]) new ArrayList[v];
+        for (int i = 0; i < v; i++)
+            graph[i] = new ArrayList<Integer>();
+    }
+
+    
+    public boolean containsCycle() {
+        int[] vertices =  new int[graph.length];
+        Arrays.fill(vertices, UNUSED);
+
+        for (int v = 0; v < graph.length; v++) {
+            if (containsCycle(v, vertices))
+                return true;
+        }
+        return false;
+    }
+
+    
+    private boolean containsCycle(int vertex, int[] vertices) {
+        switch (vertices[vertex]) {
+            case USING:  
+                return true;
+            case COMPLETE:       
+                return false;
+            case UNUSED:
+                vertices[vertex] = USING;
+        }
+
+        ArrayList<Integer> adjList = graph[vertex]; 
+        for (int v : adjList) { 
+            if (containsCycle(v, vertices))
+                return true;
+        }
+        vertices[vertex] = COMPLETE; 
+        return false;
+    }
+
+   
+    public ArrayList<Integer> LCA(int v1, int v2) {
+        ArrayList<Integer> LCA = new ArrayList<Integer>();
+        if (v1 == v2) {
+            LCA.add(v1);
+            return LCA;
+        }
+        if (v1 < 0 || v1 >= graph.length || v2 < 0 || v2 >= graph.length) {
+            return null;
+        }
+
+        ArrayList<Integer>[] parentTable = reverse(graph);
+
+        boolean[] v1Ancestors = new boolean[graph.length];
+        Arrays.fill(v1Ancestors, false);
+
+        v1Ancestors[v1] = true;
+        for (int v : parentTable[v1]) {
+            FindAnc(parentTable, v1Ancestors, v);
+        }
+
+        Queue<Integer> currentLevel = new LinkedList<Integer>();
+        Queue<Integer> nextLevel = new LinkedList<Integer>();
+        for (int v : parentTable[v2])
+            currentLevel.add(v);
+
+        while (!currentLevel.isEmpty()) {
+            while (!currentLevel.isEmpty()) {
+                int v = currentLevel.remove();
+
+                if (v1Ancestors[v])
+                    LCA.add(v);
+
+                if (LCA.isEmpty()) {
+                    for (int w : parentTable[v])
+                        nextLevel.add(w);
+                }
+            }
+            currentLevel = nextLevel;
+            nextLevel = new LinkedList<Integer>();
+        }
+
+        return LCA;
+    }
+
+   
+    private void FindAnc(ArrayList<Integer>[] parentTable, boolean[] v1Ancestors, int vertex) {
+        v1Ancestors[vertex] = true;
+        for (int v : parentTable[vertex]) {
+            FindAnc(parentTable, v1Ancestors, v);
+        }
+    }
+
+    
+    private ArrayList<Integer>[] reverse(ArrayList<Integer>[] graph) {
+        ArrayList<Integer>[] reversed = (ArrayList<Integer>[]) new ArrayList[graph.length];
+        for (int i = 0; i < reversed.length; i++)
+            reversed[i] = new ArrayList<Integer>();
+        
+        for (int v = 0; v < graph.length; v++) {
+            for (int w : graph[v])
+                Edge(reversed, w, v);
+        }
+        return reversed;
+    }
+
+   
+    public String toString() {
+        return toString(graph);
+    }
+
+    private String toString(ArrayList<Integer>[] graph) {
+        String string = "";
+        for (int v = 0; v < graph.length; v++) {
+            string += v + ", ";
+            for (int vertex : graph[v]) 
+                string += vertex + " ";
+        }
+        return string;
+    }
+
 }
--- LowestCommonAncestor/src/LowComAncTest.java
+++ LowestCommonAncestor/src/LowComAncTest.java
@@ -1,40 +1,38 @@
 import static org.junit.Assert.*;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import org.junit.Test;
 
 
 public class LowComAncTest {
+	
+	@Test
+	public void EdgeTest(){
+		LowComAnc test;
+		
+		test = new LowComAnc(5);
+		assertEquals("0, 1, 2, 3, 4, ", test.toString());
+		test.Edge(1, 3);
+		test.Edge(1, 4);
+		assertEquals("0, 1, 3 4 2, 3, 4, ", test.toString());
+	
+	}
+		
+	
+	@Test
+	public void LowComAncTest(){
+		LowComAnc test;
+		
+		test = new LowComAnc(1);
+		assertEquals("0, ", test.toString());
+		
+		test = new LowComAnc(0);
+		assertEquals("",test.toString());
+		
+		test = new LowComAnc(2);
+		assertEquals("0, 1, ", test.toString());
+		
 
-	@Test	
-	public void testFindLCA(){
-		LowComAnc tree = new LowComAnc(); 
-		//Testing empty tree, should return -1
-		assertEquals("Testing LCA on empty tree",-1, tree.findLCA(1, 2));
-		
-		tree.root = new Node(1);
-		//test on non present nodes, should return -1
-		assertEquals("Testing LCA on non present",-1, tree.findLCA(2,1));
-		
-		LowComAnc tree2 = new LowComAnc();
-		//test on multi-node tree
-        tree2.root = new Node(1);
-        tree2.root.left = new Node(2);
-        tree2.root.right = new Node(3);
-        tree2.root.left.left = new Node(4);
-        tree2.root.left.right = new Node(5);
-        tree2.root.right.left = new Node(6);
-        tree2.root.right.right = new Node(7);
-        
-        assertEquals("Testing multi-node tree", 2, (int)tree2.findLCA(4,5));
-	    assertEquals("Testing multi-node tree", 1, (int)tree2.findLCA(4,6));
-	    assertEquals("Testing multi-node tree", -1, (int)tree2.findLCA(4,8));
-	    assertEquals("Testing multi-node tree", 2, (int)tree2.findLCA(2,5));
-	    assertEquals("Testing multi-node tree", 3, (int)tree2.findLCA(6,7));
-		   
 	}
 		
 }
-
